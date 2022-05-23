2a6ee7974dfdefd352e9cb496f9b3dc36e36e907
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-23 16:11:24 +0600

    added

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -1,35 +1,36 @@
 import React from "react";
-import appointment from "../../assets/images/contact.jpg"
-// import PrimaryButton from "../Shared/PrimaryButton";
+import appointment from '../../assets/images/contact.jpg';
+import PrimaryButton from "../Shared/PrimaryButton";
 
 const Contact = () => {
     return (
-        <div style={{
-            background: `url(${appointment})`
-        }} className='bg-primary px-10 py-14 '>
-            <div className='text-center pb-14 text-white'>
-                <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
-                    Contact Us
-                </p>
-                <h1 className='text-4xl'>Stay connected with us</h1>
-            </div>
-            <div className='grid grid-cols-1 justify-items-center gap-5'>
-                <input
-                    type='text'
-                    placeholder='Email Address'
-                    className='input w-full max-w-md'
-                />
-                <input
-                    type='text'
-                    placeholder='Subject'
-                    className='input w-full max-w-md'
-                />
-                <textarea
-                    className='textarea w-full max-w-md'
-                    placeholder='Your message'
-                    rows={6}
-                ></textarea>
-                {/* <PrimaryButton>Submit</PrimaryButton> */}
+        <div>
+            <h2 className='my-12 text-2xl lg:text-4xl font-bold text-center text-primary'>Our available products</h2>
+            <div style={{
+                background: `url(${appointment})`
+            }} className='bg-primary px-10 py-14 w-full'>
+                <div className='text-center pb-14 text-white'>
+                    
+                    <h1 className='text-4xl'>Stay connected with us</h1>
+                </div>
+                <div className='grid grid-cols-1 justify-items-center gap-5'>
+                    <input
+                        type='text'
+                        placeholder='Email Address'
+                        className='input w-full max-w-md'
+                    />
+                    <input
+                        type='text'
+                        placeholder='Subject'
+                        className='input w-full max-w-md'
+                    />
+                    <textarea
+                        className='textarea w-full max-w-md'
+                        placeholder='Your message'
+                        rows={6}
+                    ></textarea>
+                    <PrimaryButton>Submit</PrimaryButton>
+                </div>
             </div>
         </div>
     );
