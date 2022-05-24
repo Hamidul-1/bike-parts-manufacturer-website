ace6a870b1efc105618bcdd1d391af2e0850aace
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-24 21:45:03 +0600

    added

--- src/Pages/Home/Part.js
+++ src/Pages/Home/Part.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Part = ({ part }) => {
-    const { name, description, price, img, min } = part;
+    const { name, description, price, img, min, quantity, availableQuantity } = part;
     return (
         <div class="card mx-auto bg-base-100 shadow-2xl">
             <figure><img src={img} className="w-48" alt="product" /></figure>
@@ -10,8 +10,10 @@ const Part = ({ part }) => {
                 <p>{description}</p>
                 <p className='text-lg'>Price: <span className='font-bold text-warning'>{price}$</span></p>
                 <span className='w-32 btn btn-xs text-xs p-1 btn-outline'>Minimum Order: {min} </span>
+                <span className='w-32 btn btn-xs text-xs p-1 btn-outline'>Quantity: {quantity} </span>
+                <span className='w-44 btn btn-xs text-xs p-1 btn-outline'>availableQuantity: {availableQuantity} </span>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Order Now</button>
+                    <button class="btn btn-primary">Purchase Now</button>
                 </div>
             </div>
         </div>
