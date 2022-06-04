e6d1fbc173850b338b69576e4b723d77a5ef66bc
Hamidul-1 <hamidul786cse@gmail.com>
2022-06-04 11:59:02 +0600

    added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={bike} class="max-w-sm rounded-lg shadow-2xl w-full" />
                 <div>
-                    <h1 class="text-5xl font-bold">Your New Style here!!!</h1>
+                    <h1 class="text-5xl font-bold">Your New Style here!</h1>
                     <p class="py-6">Some people see just a  motorbike.You see possession, joy, and lifestyle.</p>
                     <button class="btn btn-primary uppercase text-white font-bolt">Get Started</button>
                 </div>
